701283bcf98292e301148ae384de5862b5ef29c8
xhyrom <generalkubo@gmail.com>
2021-10-31 17:10:53 +0100

    fix

--- src/hyttpo.ts
+++ src/hyttpo.ts
@@ -5,12 +5,24 @@ import Utils from './utils';
 import Response from './structures/Response';
 const methods = ['GET', 'POST', 'PATCH', 'PUT', 'TRACE', 'HEAD', 'OPTIONS', 'CONNECT', 'DELETE'];
 
-type PayloadMethod = 'GET' | 'POST' | 'PATCH' | 'PUT' | 'TRACE' | 'HEAD' | 'OPTIONS' | 'CONNECT' | 'DELETE';
 type ResponseType = 'stream' | 'arraybuffer' | 'buffer' | 'text';
+
+enum PayloadMethod {
+    'GET',
+    'POST',
+    'PATCH',
+    'PUT',
+    'TRACE',
+    'HEAD',
+    'OPTIONS',
+    'CONNECT',
+    'DELETE',
+}
+
 interface PayloadRequest {
     method: PayloadMethod;
     url: string;
-    body?: object | string;
+    body?: string;
     responseType?: ResponseType;
     headers?: object;
 }
@@ -45,8 +57,8 @@ class Hyttpo {
             if (!headers['Accept']) headers['Accept'] = 'application/json, text/plain, */*';
             if (!headers['User-Agent']) headers['User-Agent'] = 'riso/nodejs'
     
-            if(['POST', 'PATCH', 'PUT', 'TRACE', 'HEAD', 'OPTIONS', 'CONNECT', 'DELETE'].includes(method)) request = request.request;
-            else request = request[method.toLowerCase()];
+            if(['POST', 'PATCH', 'PUT', 'TRACE', 'HEAD', 'OPTIONS', 'CONNECT', 'DELETE'].includes(method.toString())) request = request.request;
+            else request = request[method.toString().toLowerCase()];
 
             let agent = isHttps ? data.httpsAgent || data.agent : data.httpAgent || data.agent
 
@@ -122,7 +134,7 @@ class Hyttpo {
                 reject(error);
             })
 
-            if(body && method !== 'GET') {
+            if(body && method !== PayloadMethod.GET) {
                 if(Utils.isObject(body) && (body?.constructor?.name === 'FormData' || Utils.isStream(body))) {
                     body.pipe(req)
                 } else {

ddcc44205f9b3f3c2d50d7fe8822d69e3b0ffe72
xhyrom <generalkubo@gmail.com>
2021-12-09 17:34:27 +0100

    gh release test, webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack');
+
+module.exports = {
+    resolve: {
+        fallback: {
+            "zlib": require.resolve("browserify-zlib"),
+            "http": require.resolve("stream-http"),
+            "https": require.resolve("https-browserify"),
+            "url": require.resolve("url"),
+            "assert": require.resolve("assert"),
+            "util": require.resolve("util"),
+            "stream": require.resolve("stream-browserify"),
+            "buffer": require.resolve("buffer")
+        },
+        alias: {
+            process: "process/browser"
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+        new webpack.ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        })
+    ],
+    entry: './dist/index.js',
+    output: {
+        path: __dirname + '/dist/',
+        filename:  'hyttpo-webpack.js',
+        sourceMapFilename: 'hyttpo-webpack.map',
+        library: 'hyttpo',
+        libraryTarget: 'umd',
+        globalObject: 'this'
+    },
+    devtool: 'source-map',
+    mode: 'production'
+}

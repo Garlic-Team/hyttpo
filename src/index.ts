8b312ca11a0a5a3bdfec7bba7a20edbf6afe3f9d
Hyro <generalkubo@gmail.com>
2021-12-12 23:11:18 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Hyttpo } from './base/hyttpo';
 export { Hyttpo } from './base/hyttpo';
 export * from './util/utils';
 export * from './structures/Response';
+export * from './structures/HPromise';
 export * from './util/constants';
 
 export const hyttpo = new Hyttpo();
